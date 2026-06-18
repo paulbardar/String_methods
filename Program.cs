64de85ba37b03b897d201481e189d3bacf8cd7ba
paulbardar <Q.C.tulip@gmail.com>
2026-06-18 20:53:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter text for Encrypting: ");
+            string inputText = Console.ReadLine();
+
+            Console.Write("Enter the key (number to shift): ");
+            int shift = int.Parse(Console.ReadLine());
+
+            string encryptedText = Encrypt(inputText, shift);
+            Console.WriteLine($"\nCiphertext: {encryptedText}");
+
+            string decryptedText = Decrypt(encryptedText, shift);
+            Console.WriteLine($"Decrypted text: {decryptedText}");
+
+            Console.ReadLine();
+        }
+
+        static string Encrypt(string text, int shift)
+        {
+            char[] buffer = text.ToCharArray();
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+
+                if (char.IsLetter(letter))
+                {
+                    char offset = char.IsUpper(letter) ? 'A' : 'a';
+
+                    letter = (char)((((letter + shift) - offset) % 26) + offset);
+                }
+                buffer[i] = letter;
+            }
+
+            return new string(buffer);
+        }
+
+        static string Decrypt(string text, int shift)
+        {
+            return Encrypt(text, 26 - shift);
+        }
+    }
+}
